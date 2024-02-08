586fabf03e72f8e14dfb5e4e0087c8ac591d7d46
Nataliia Kubytska <natashakub27@gmail.com>
2024-02-09 00:24:12 +0200

    fixed naming

--- src/config/wdioSequential.conf.js
+++ src/config/wdioSequential.conf.js
@@ -20,7 +20,7 @@ exports.config = {
   // The path of the spec files will be resolved relative from the directory of
   // of the config file unless it's absolute.
   //
-  specs: ["../test/specs/concurrent/**/*.js"],
+  specs: ["../test/specs/sequential/**/*.js"],
   // Patterns to exclude.
   exclude: [
     // 'path/to/excluded/files'
