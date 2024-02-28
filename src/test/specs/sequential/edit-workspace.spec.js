855025f07ae6c0e0216b484ef57424b982ff198f
Nataliia Kubytska <natashakub27@gmail.com>
2024-02-28 22:15:03 +0200

    uncommented edit-workspace

--- src/test/specs/sequential/edit-workspace.spec.js
+++ src/test/specs/sequential/edit-workspace.spec.js
@@ -1,94 +1,94 @@
-// const { expect } = require("chai");
-// const { pages } = require("../../../page");
-// const { hooksBeforeEach, hooksAfter } = require("../../../support/hooks");
-// const resources = require("../../../support/resources");
+const { expect } = require("chai");
+const { pages } = require("../../../page");
+const { hooksBeforeEach, hooksAfter } = require("../../../support/hooks");
+const resources = require("../../../support/resources");
 
-// describe("Trello editing workspace", () => {
-//   beforeEach(hooksBeforeEach.loginAndOpenWorkspacePage);
+describe("Trello editing workspace", () => {
+  beforeEach(hooksBeforeEach.loginAndOpenWorkspacePage);
 
-//   describe("Update workspace name with valid characters", () => {
-//     it("should update workspace name when edited using valid characters", async () => {
-//       await pages(
-//         "workspace"
-//       ).workspaceInfo.openEditWorkspaceFormBtn.waitAndClick();
+  describe("Update workspace name with valid characters", () => {
+    it("should update workspace name when edited using valid characters", async () => {
+      await pages(
+        "workspace"
+      ).workspaceInfo.openEditWorkspaceFormBtn.waitAndClick();
 
-//       const editWorkspaceForm = await pages("workspace").editWorkspaceForm;
-//       await editWorkspaceForm
-//         .input("name")
-//         .waitAndAddValue(resources.testingInputValue);
-//       await editWorkspaceForm.saveEditBtn.waitAndClick();
+      const editWorkspaceForm = await pages("workspace").editWorkspaceForm;
+      await editWorkspaceForm
+        .input("name")
+        .waitAndAddValue(resources.testingInputValue);
+      await editWorkspaceForm.saveEditBtn.waitAndClick();
 
-//       const workspaceInfo = await pages("workspace").workspaceInfo;
-//       const workspaceName = await workspaceInfo.waitAndGetText(
-//         workspaceInfo.workspaceName
-//       );
+      const workspaceInfo = await pages("workspace").workspaceInfo;
+      const workspaceName = await workspaceInfo.waitAndGetText(
+        workspaceInfo.workspaceName
+      );
 
-//       //using chai Expect
-//       await expect(workspaceName).to.have.string(resources.testingInputValue);
-//     });
+      //using chai Expect
+      await expect(workspaceName).to.have.string(resources.testingInputValue);
+    });
 
-//     after(hooksAfter.after.resetWorkspaceName);
-//   });
+    after(hooksAfter.after.resetWorkspaceName);
+  });
 
-//   describe("Update workspace description with valid characters", () => {
-//     it("should update workspace name when edited using valid characters", async () => {
-//       await pages(
-//         "workspace"
-//       ).workspaceInfo.openEditWorkspaceFormBtn.waitAndClick();
+  describe("Update workspace description with valid characters", () => {
+    it("should update workspace name when edited using valid characters", async () => {
+      await pages(
+        "workspace"
+      ).workspaceInfo.openEditWorkspaceFormBtn.waitAndClick();
 
-//       const editWorkspaceForm = await pages("workspace").editWorkspaceForm;
-//       await editWorkspaceForm
-//         .input("description")
-//         .waitAndAddValue(resources.testingInputValue);
-//       await editWorkspaceForm.saveEditBtn.waitAndClick();
+      const editWorkspaceForm = await pages("workspace").editWorkspaceForm;
+      await editWorkspaceForm
+        .input("description")
+        .waitAndAddValue(resources.testingInputValue);
+      await editWorkspaceForm.saveEditBtn.waitAndClick();
 
-//       const workspaceInfo = pages("workspace").workspaceInfo;
-//       const workspaceDescription = await workspaceInfo.waitAndGetText(
-//         workspaceInfo.workspaceDescription
-//       );
+      const workspaceInfo = pages("workspace").workspaceInfo;
+      const workspaceDescription = await workspaceInfo.waitAndGetText(
+        workspaceInfo.workspaceDescription
+      );
 
-//       //using chai Expect
-//       await expect(
-//         workspaceInfo.workspaceDescription,
-//         "description did not exist"
-//       ).to.exist;
-//       await expect(workspaceDescription).to.equal(resources.testingInputValue);
-//     });
+      //using chai Expect
+      await expect(
+        workspaceInfo.workspaceDescription,
+        "description did not exist"
+      ).to.exist;
+      await expect(workspaceDescription).to.equal(resources.testingInputValue);
+    });
 
-//     after(hooksAfter.after.resetWorkspaceDescription);
-//   });
+    after(hooksAfter.after.resetWorkspaceDescription);
+  });
 
-//   describe("cancel workspace editing", async () => {
-//     it("workspace name should remain unchanged when cancel edited changes", async () => {
-//       const workspaceInfo = await pages("workspace").workspaceInfo;
-//       const initialWorkspaceName = await workspaceInfo.waitAndGetText(
-//         workspaceInfo.workspaceName
-//       );
+  describe("cancel workspace editing", async () => {
+    it("workspace name should remain unchanged when cancel edited changes", async () => {
+      const workspaceInfo = await pages("workspace").workspaceInfo;
+      const initialWorkspaceName = await workspaceInfo.waitAndGetText(
+        workspaceInfo.workspaceName
+      );
 
-//       await pages(
-//         "workspace"
-//       ).workspaceInfo.openEditWorkspaceFormBtn.waitAndClick();
+      await pages(
+        "workspace"
+      ).workspaceInfo.openEditWorkspaceFormBtn.waitAndClick();
 
-//       const editWorkspaceForm = await pages("workspace").editWorkspaceForm;
-//       await editWorkspaceForm
-//         .input("description")
-//         .waitAndAddValue(resources.testingInputValue);
-//       await editWorkspaceForm.cancelEditBtn.waitAndClick();
+      const editWorkspaceForm = await pages("workspace").editWorkspaceForm;
+      await editWorkspaceForm
+        .input("description")
+        .waitAndAddValue(resources.testingInputValue);
+      await editWorkspaceForm.cancelEditBtn.waitAndClick();
 
-//       const finalWorkspaceName = await workspaceInfo.waitAndGetText(
-//         workspaceInfo.workspaceName
-//       );
+      const finalWorkspaceName = await workspaceInfo.waitAndGetText(
+        workspaceInfo.workspaceName
+      );
 
-//       //using chai Expect
-//       await expect(initialWorkspaceName).to.equal(
-//         finalWorkspaceName,
-//         "workspace name changed"
-//       );
-//       await expect(finalWorkspaceName).to.not.include(
-//         resources.testingInputValue
-//       );
-//     });
+      //using chai Expect
+      await expect(initialWorkspaceName).to.equal(
+        finalWorkspaceName,
+        "workspace name changed"
+      );
+      await expect(finalWorkspaceName).to.not.include(
+        resources.testingInputValue
+      );
+    });
 
-//     afterEach(hooksAfter.afterEach.reload);
-//   });
-// });
+    afterEach(hooksAfter.afterEach.reload);
+  });
+});
